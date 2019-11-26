a2310bd62a4f354365a75c235ca3835ce09240bf
Cassie Achzenick <caachzenick@gmail.com>
2019-11-26 15:46:47 -0700

    Add finished iteration 3 and most of iteration 4

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,11 +6,9 @@ class LinkedList
 
   def initialize
     @head = head
-    @count = 0
   end
 
   def append(surname)
-    @count += 1
     if @head == nil
       @head = Node.new(surname)
     else
@@ -18,15 +16,102 @@ class LinkedList
     end
   end
 
+  def prepend(surname)
+    if @head.next_node != nil
+      new_head = Node.new(surname)
+      new_head.next_node = @head
+      @head = new_head
+    end
+  end
+
+  def insert(position, surname)
+    mover = @head
+    output = @head
+    count = 0
+    new_family = Node.new(surname)
+    until mover == nil
+      if count == position
+        new_family.next_node = mover
+        if count == 1
+          output.next_node = new_family
+        elsif count == 2
+          output.next_node.next_node = new_family
+        elsif count == 3
+          output.next_node.next_node.next_node = new_family
+        elsif count == 4
+          output.next_node.next_node.next_node.next_node = new_family
+        elsif count == 5
+          output.next_node.next_node.next_node.next_node.next_node = new_family
+        end
+        break
+      end
+      count += 1
+      mover = mover.next_node
+    end
+    @head = output
+  end
+
   def count
-    @count
+    count = 0
+    mover = @head
+    until mover == nil
+      count += 1
+      mover = mover.next_node
+    end
+    count
   end
 
   def to_string
-    if @head.next_node == nil
-      "The #{@head.surname} family"
-    else
-      "The #{@head.surname} family, followed by the #{@head.next_node.surname} family."
+    string = "The #{@head.surname} family"
+    mover = head.next_node
+    until mover == nil
+      string = string + ", followed by the #{mover.surname} family"
+      mover = mover.next_node
     end
+    string
+  end
+
+  def find(return_position, number_of_elements)
+    mover = @head
+    counter = 0
+    until mover == nil
+      if counter == return_position - 1
+        if number_of_elements == 1
+          return "The #{mover.next_node.surname} family"
+        elsif number_of_elements == 2
+          return "The #{mover.next_node.surname} family, followed by the #{mover.next_node.next_node.surname} family"
+        elsif number_of_elements == 3
+          return "The #{mover.next_node.surname} family, followed by the #{mover.next_node.next_node.surname} family, followed by the #{mover.next_node.next_node.next_node.surname} family"
+        end
+      end
+      counter += 1
+      mover = mover.next_node
+    end
+  end
+
+  def includes?(search_key)
+    mover = @head
+    counter = 0
+    status = false
+    until mover == nil
+      if mover.surname == search_key
+        status = true
+      end
+    counter += 1
+    mover = mover.next_node
+    end
+    status
+  end
+
+  def pop
+    mover = @head
+    counter = 0
+    status = false
+    until mover == nil
+      p counter
+      counter += 1
+      mover = mover.next_node
+    end
+    total = counter
   end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -47,6 +47,86 @@ class LinkedListTest < Minitest::Test
     family_rhodes = list.append("Rhodes")
     family_hardy = list.append("Hardy")
 
-    assert_equal 2, list
+    assert_equal family_hardy, family_rhodes.next_node
+  end
+
+  def test_new_family_can_be_prepended
+    list = LinkedList.new
+    family_brooks = list.append("Brooks")
+    family_henderson = list.append("Henderson")
+    family_mckinney = list.prepend("McKinney")
+    assert_equal family_brooks, family_mckinney.next_node
+  end
+
+  def test_it_can_print_out_wagon_travel_chain_as_string
+    list = LinkedList.new
+    family_brooks = list.append("Brooks")
+    family_henderson = list.append("Henderson")
+    family_mckinney = list.prepend("McKinney")
+
+    assert_equal"The McKinney family, followed by the Brooks family, followed by the Henderson family", list.to_string
+  end
+
+  def test_it_can_count_multiple_familys
+    list = LinkedList.new
+    family_brooks = list.append("Brooks")
+    family_henderson = list.append("Henderson")
+    family_mckinney = list.prepend("McKinney")
+
+    assert_equal 3, list.count
+  end
+
+  def test_new_family_can_be_inserted_anywhere
+    list = LinkedList.new
+    family_brooks = list.append("Brooks")
+    family_henderson = list.append("Henderson")
+    family_mckinney = list.prepend("McKinney")
+    family_lawson = list.insert(1, "Lawson")
+
+    assert_equal "The McKinney family, followed by the Lawson family, followed by the Brooks family, followed by the Henderson family", list.to_string
+  end
+
+  def test_it_can_insert_new_family_in_a_second_location
+    list = LinkedList.new
+    family_brooks = list.append("Brooks")
+    family_henderson = list.append("Henderson")
+    family_mckinney = list.prepend("McKinney")
+    family_lawson = list.insert(2, "Lawson")
+
+    assert_equal "The McKinney family, followed by the Brooks family, followed by the Lawson family, followed by the Henderson family", list.to_string
+  end
+
+  def test_it_has_a_find_function
+    list = LinkedList.new
+    family_brooks = list.append("Brooks")
+    family_henderson = list.append("Henderson")
+    family_mckinney = list.prepend("McKinney")
+    family_lawson = list.insert(1, "Lawson")
+
+    assert_equal "The Brooks family", list.find(2, 1)
+    assert_equal "The Lawson family, followed by the Brooks family, followed by the Henderson family", list.find(1, 3)
+  end
+
+  def test_it_can_include_a_variable
+    list = LinkedList.new
+    family_brooks = list.append("Brooks")
+    family_henderson = list.append("Henderson")
+    family_mckinney = list.prepend("McKinney")
+    family_lawson = list.insert(1, "Lawson")
+
+    assert_equal true, list.includes?("Brooks")
+    assert_equal false, list.includes?("Chapman")
+  end
+
+  def test_a_family_can_die_from_disentry
+    list = LinkedList.new
+    family_brooks = list.append("Brooks")
+    family_henderson = list.append("Henderson")
+    family_mckinney = list.prepend("McKinney")
+    family_lawson = list.insert(1, "Lawson")
+
+    assert_equal 'The Henderson family has died of dysentery', list.pop
+    assert_equal 'The Brooks family has died of dysentery', list.pop
+    assert_equal "The McKinney family, followed by the Lawson family", list.to_string
   end
 end
