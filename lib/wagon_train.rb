c256c5820d21a7a0b16825677d53b3a69af73f9c
Cassie Achzenick <caachzenick@gmail.com>
2019-11-27 13:21:35 -0700

    Add working go_hunting

--- lib/wagon_train.rb
+++ lib/wagon_train.rb
@@ -30,39 +30,23 @@ class WagonTrain
   end
 
   def go_hunting
-    animals = [{"squirrel" => [2]}, {"deer" => [40]}, {"bison" => [100]}]
-    animals_hunted = []
-    total_animals_hunted = rand(5)
-    total_food = {}
-    output = ""
+    random_number = rand(5)
+    game_got = []
+    potential_game = [{"squirrel" => 2}, {"deer" => 40}, {"bison" => 100}]
+    random_number.times do
+      game_got << potential_game[rand(3)]
+    end
+    total_squirrels = game_got.count({"squirrel" => 2})
+    how_say_squirrle = "squirrel"
+    total_deer = game_got.count({"deer" => 40})
+    total_bison = game_got.count({"bison" => 100})
+    if total_squirrels > 1
+      how_say_squirrle = "squirrels"
+    end
+    total_food_from_game = {"pounds of food" => 0}
+    total_food_from_game["pounds of food"] = (total_squirrels * 2) + (total_deer * 40) + (total_bison * 100)
+    @supplies = @supplies.merge(total_food_from_game) {|key, oldval, newval| newval + oldval}
+    "You got #{total_squirrels} #{how_say_squirrle}, #{total_deer} deer and #{total_bison} for #{total_food_from_game["pounds of food"]} pounds of food"
     require "pry"; binding.pry
-    total_animals_hunted.times do
-      animals_hunted << animals[rand(2)]
-    end
-    animals_hunted.each do |animal|
-      total_food = total_food.merge(animal) {|key, oldval1, newval1| newval1 + oldval1}
-    end
-    if total_food["squirrel"] != nil && total_food["squirrel"].length > 1
-      total_food["squirrels"] = total_food.delete("squirrel")
-    end
-    sum_of_all_food = 0
-    total_food.each do |food|
-      if food == total_food.first
-        sum_of_all_food = sum_of_all_food + (food[1].length * food[1][0])
-        output = "You got #{food[1].length} #{food[0]} "
-      elsif food[0] == total_food.keys.last
-        sum_of_all_food = sum_of_all_food + (food[1].length * food[1][0])
-        output = output + "and #{food[1].length} #{food[0]} for #{sum_of_all_food} pounds of food"
-      else
-        sum_of_all_food = sum_of_all_food + (food[1].length * food[1][0])
-        output = output + ", #{food[1].length} #{food[0]}"
-      end
-    end
-    if @supplies["pounds of food"] == nil
-      @supplies["pounds of food"] = sum_of_all_food
-    else
-      @supplies["pounds of food"] = @supplies["pounds of food"] + sum_of_all_food
-    end
-    p output
   end
 end
